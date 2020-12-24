fef32e66fb48cbe47fc0a05a3f3e6da6c5854aac
Kasper Kuzmicki <kasper.kuzmicki@yahoo.com>
2020-12-23 22:47:54 -0500

    Another commit.

--- Assets/js/Highscores.js
+++ Assets/js/Highscores.js
@@ -1,7 +1,4 @@
 $('document').ready(function(){
 let userScores = document.querySelector("#user-scores");
 userScores.textContent = JSON.parse(localStorage.getItem("Scores"))
-const highScores = JSON.parse(localStorage.getItem('Scores'));
-for ()
 });
-
